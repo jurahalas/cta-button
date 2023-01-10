453dd92fd6d2b1a72014e0dfe1f612f4b1a6636e
Yuriy Halas <jurahalas@gmail.com>
2023-01-10 17:22:52 +0200

    cta btns

---
+++ src/buttons/fabButtonNew/Button.tsx
@@ -0,0 +1,33 @@
+import { FC, PropsWithChildren, useState } from "react";
+import "./styles.css";
+import {LaunchButton} from "../launchButton/LaunchButton";
+
+const Icon: FC<PropsWithChildren> = ({ children }) => (
+    <i className="material-symbols-outlined">{children}</i>
+);
+
+export const Button = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    return (
+        <div className={`fab ${isOpen ? "open" : ""}`}>
+            <button onClick={() => setIsOpen(!isOpen)}>
+                <Icon>add</Icon>
+            </button>
+            <div className="menu">
+                <button>
+                    <Icon>person_add</Icon>
+                    <span>Add Users</span>
+                </button>
+                <button>
+                    <Icon>description</Icon>
+                    <span>Documents</span>
+                </button>
+                <button>
+                    <Icon>settings</Icon>
+                    <span>Settings</span>
+                </button>
+            </div>
+        </div>
+    );
+};
